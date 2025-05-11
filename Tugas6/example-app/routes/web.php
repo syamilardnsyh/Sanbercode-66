23abe939c533eb5820d010efcdcf68afc1d1fef4
syamilardnsyh <syamilagni09@gmail.com>
2025-05-12 00:36:05 +0700

    ...

--- Tugas6/example-app/routes/web.php
+++ Tugas6/example-app/routes/web.php
@@ -4,9 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DaftarController;
 use App\Http\Controllers\SelamatController;
+use App\Http\Controllers\CategoryController;
 
 
 Route::get('/', [DashboardController::class, 'home']);
 Route::get('/daftar', [DaftarController::class, 'pendaftaran']);
 
 Route::get('/selamat', [SelamatController::class, 'selamat']);
+
+//ROUTE CATEGORIES / GENRES
+// CREATE DATA
+Route::get('category/create', [CategoryController::class, 'create']);
